e5f13b43099b27243a5c9269c4071e42ccae90e1
Margo Lanier <margo.lanier@agencyq.com>
2016-09-26 18:05:46 -0400

    basic page layout, added feed js and simple styles

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-$(document).foundation();
+$(document).ready(function() {
+    
+    $(document).foundation();
+
+    // Generate RSS Feeds
+	$('#rss-feed').FeedEk({
+		FeedUrl: 'http://jquery-plugins.net/rss',
+		MaxCount: 10,
+		DescCharacterLimit: 180,
+		DateFormat: 'lll',
+		DateFormatLang:'en'
+	});
+
+	// Sort by date, toggle asc and desc
+	$('li').sort(function(a, b) {
+		var aDate = $(a).find('.itemDate').text();
+		var bDate = $(b).find('.itemDate').text();
+		return new Date(bDate).getTime() - new Date(aDate).getTime();
+	}).appendTo('#divRss ul');
+
+
+	
+});
